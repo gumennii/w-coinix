f25a07efa35d22f402ae010a08f440d73df1d059
Alexei <leha.pol1996@mail.ru>
2024-08-29 22:17:19 +0300

    replace hard links

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: 'w-coinix',
+  base: '',
   root: 'src',
   build: {
     outDir: '../dist',
