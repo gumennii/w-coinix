36d22d05e17a83151cb00378ee8134306cea082e
Alexei <leha.pol1996@mail.ru>
2024-08-29 20:42:38 +0300

    add yml to create github action

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '',
+  base: 'w-coinix',
   root: 'src',
   build: {
     outDir: '../dist',
