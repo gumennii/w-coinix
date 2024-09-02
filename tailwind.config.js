6e1db4ae1437cd9b61368f59920fd033738ab7a4
Alexei <leha.pol1996@mail.ru>
2024-09-02 21:42:31 +0300

    make line-height bigger in tw conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,15 @@ const config = {
       white: '#ffffff',
       darkBlue: '#011424',
     },
+    fontSize: {
+      base: ['1rem', '1.75rem'],
+      lg: ['1.125rem', '2rem'],
+      xl: ['1.25rem', '2rem'],
+      '3xl': ['1.875rem', '2.75rem'],
+      '4xl': ['2.25rem', '3rem'],
+      '5xl': ['3rem', '1'],
+      '7xl': ['4.5rem', '1'],
+    },
     extend: {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
